3f8c15e88d7ba2bf38ccc4114828ea18b0980c17
Y Kim <youngsu.kim@csusb.edu>
2023-08-25 12:14:14 -0700

    Add readme

---
+++ docs/conf.py
@@ -0,0 +1,35 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'MATH 6000 Communicating Matheamtics'
+copyright = '2022-2023, Youngsu Kim'
+author = 'Youngsu Kim'
+release = 'v1'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+# extensions = [
+#     "sphinx_rtd_theme",
+#     # "myst_parser",
+#     # "sphinx_math_dollar",
+# ]
+
+templates_path = ['_templates']
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store', 'source/*.rst']
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# html_theme = 'alabaster'
+# html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_book_theme'
+
+html_static_path = ['_static']
